ca548c284055971741a0099a2cecda7f6e2739ae
Silvia Donaire <silviadonaireserrano@gmail.com>
2024-01-18 12:23:22 +0100

    feat: add numbers of weights and inputs

--- src/app.py
+++ src/app.py
@@ -20,17 +20,22 @@ pesos = []
 entradas = []
 
 # Capturar los valores de los pesos y las entradas
+st.subheader("Pesos")
 columnas_pesos = st.columns(num_entradas)
-columnas_entradas = st.columns(num_entradas)
 for i in range(num_entradas):
     with columnas_pesos[i]:
         peso = st.number_input(f"w{i}", key=f"w{i}")
         pesos.append(peso)
+st.text(f"w = {pesos}")
 
+st.subheader("Entradas")
+columnas_entradas = st.columns(num_entradas)
+for i in range(num_entradas):
     with columnas_entradas[i]:
         entrada = st.number_input(f"x{i}", key=f"x{i}")
         entradas.append(entrada)
-    
+st.text(f"s = {entradas}")
+
 col1, col2 = st.columns(2)
 with col1:
     st.subheader("Sesgo")
